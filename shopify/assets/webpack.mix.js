07ed07449c13e6f7152cd89a42803f711abe9c20
diamantelezi25 <diamant@solution25.com>
2022-05-18 16:29:46 +0200

    Installion of custom theme kit
    installed Tailwind css , Alpine js , Swiper , jQuery

---
+++ shopify/assets/webpack.mix.js
@@ -0,0 +1,22 @@
+// webpack.mix.js
+let mix = require('laravel-mix');
+mix.copy('tailwind.config.js', 'shopify/assets/tailwind.config.js');
+mix.copy('webpack.mix.js', 'shopify/assets/webpack.mix.js');
+mix.styles(
+    [
+        'dist/css/application.css',
+        'node_modules/swiper/swiper-bundle.min.css'
+    ],
+    'shopify/assets/solution25.css'
+)
+
+mix.scripts(
+    [
+        "node_modules/jquery/dist/jquery.min.js",
+        "node_modules/alpinejs/dist/cdn.min.js",
+        "node_modules/swiper/swiper-bundle.min.js",
+        "node_modules/lazysizes/lazysizes.min.js",
+        // "dist/js/custom.js"
+    ],
+    "shopify/assets/main.js"
+)
